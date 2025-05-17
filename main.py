2e2a2c7a75ad7d4ba060d1bf938d9f302676a54c
SyedSafdarAliShah <programmersafdar@live.com>
2025-05-17 13:01:29 +0500

    agents methods and fastapi endpoint setup completed

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from fastapi import FastAPI
+app = FastAPI()
+
+def get_agent_response(message: str):
+    return f"user asked about {message} and Agent Response is this =====>"
+
+@app.get("/")
+async def home():
+    return {"message": "Welcome to AI Startup Tech Advisor build on Open AI Agent SDK"}
+@app.get("/{message}")
+async def hello(message: str):
+    result = get_agent_response(message)
+    return {"user_message": message,"agent_response": result}
