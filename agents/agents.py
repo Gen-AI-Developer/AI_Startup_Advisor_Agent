507ec614c8d78706f94cf56013f0f891ae217bc4
SyedSafdarAliShah <programmersafdar@live.com>
2025-05-17 23:19:26 +0500

    api added

--- agents/agents.py
+++ agents/agents.py
@@ -1,2 +1,4 @@
 from agents import agent, set_tracing_disabled, function_tool 
 from agents.extensions.models.litellm_model import LitellmModel
+import os
+gemini_api_key = os.getenv('GEMINI_API_KEY')
