fb19749780e16e1995ac06b4d99aa5ada54eca5e
SyedSafdarAliShah <programmersafdar@live.com>
2025-05-18 07:57:05 +0500

    working on agents

--- agents/agents.py
+++ agents/agents.py
@@ -1,4 +1,13 @@
-from agents import agent, set_tracing_disabled, function_tool 
+from agents import Agent, set_tracing_disabled, function_tool 
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 gemini_api_key = os.getenv('GEMINI_API_KEY')
+
+agent : Agent = Agent(
+    name="Assitant",
+    instructions="You Are Helpfull Assistant, you answer every question will short and breif to the point answer.",
+    model=LitellmModel(model="gemini/gemini-2.0-flash",api_key=gemini_api_key),
+    )
+
+def agent_response(user_query:str):
+    pass
