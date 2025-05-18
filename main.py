11c8725f7bb894e4f13b5c5354a196e66768d96a
SyedSafdarAliShah <programmersafdar@live.com>
2025-05-18 09:20:28 +0500

    update

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from fastapi import FastAPI
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
+from myagents import myagents
+from pydantic import BaseModel
+from agents import Agent, set_tracing_disabled, function_tool, Runner
+from agents.extensions.models.litellm_model import LitellmModel
+import os
+gemini_api_key = os.getenv('GEMINI_API_KEY')
+set_tracing_disabled(disabled=True)
+agent : Agent = Agent(
+    name="Assitant",
+    instructions="You Are Helpfull Assistant, you answer every question will short and breif to the point answer.",
+    model=LitellmModel(model="gemini/gemini-2.0-flash",api_key=gemini_api_key),
+    )
 
-def get_agent_response(message: str):
-    return f"user asked about {message} and Agent Response is this =====>"
+async def agent_response(user_query:str):
+    result = await Runner.run(agent,user_query)
+    return result.final_output
+
+app = FastAPI(
+    title="AI Startup Tech Advisor",
+    description="An AI-powered advisor for startup technology decisions",
+    version="1.0.0"
+)
+
 
 @app.get("/")
-async def home():
-    return {"message": "Welcome to AI Startup Tech Advisor build on Open AI Agent SDK"}
+async def root():
+    return {"message": "Welcome to AI Startup Tech Advisor API"}
+
 @app.get("/{message}")
-async def hello(message: str):
-    result = get_agent_response(message)
-    return {"user_message": message,"agent_response": result}
+async def agentic_response(message:str):
+    result = await agent_response(message)
+    return {"RESPONSE":result}
