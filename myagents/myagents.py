3093baa6594b75ff21c56e6dbe88b7b24ac59e67
SyedSafdarAliShah <programmersafdar@live.com>
2025-05-18 10:43:04 +0500

    4 agents added

--- myagents/myagents.py
+++ myagents/myagents.py
@@ -8,7 +8,26 @@ agent : Agent = Agent(
     instructions="You Are Helpfull Assistant, you answer every question will short and breif to the point answer.",
     model=LitellmModel(model="gemini/gemini-2.0-flash",api_key=gemini_api_key),
     )
-
+business_model_dev_analyst_agent : Agent = Agent(
+    name="Business Model Development Analyst",
+    instructions="",
+    model=LitellmModel(model="gemini/gemini-2.0-flash",api_key=gemini_api_key),
+    )
+swot_analysis_agent : Agent = Agent(
+    name="SWOT Analysis Agent",
+    instructions="",
+    model=LitellmModel(model="gemini/gemini-2.0-flash",api_key=gemini_api_key),
+    )
+gtm_strategy_agent : Agent = Agent(
+    name="GTM Strategy Analyst Agent",
+    instructions="",
+    model=LitellmModel(model="gemini/gemini-2.0-flash",api_key=gemini_api_key),
+    )
+tech_stack_agent : Agent = Agent(
+    name="Tech Stack Analyst Agent",
+    instructions="",
+    model=LitellmModel(model="gemini/gemini-2.0-flash",api_key=gemini_api_key),
+    )
 async def general_agent_response(user_query:str):
     result = await Runner.run(agent,user_query)
     return result.final_output
